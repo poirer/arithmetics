0b3c47ad7d49a4baaf88254832ab7af71f3fb8dd
poirer <poire.raisonnable@gmail.com>
2016-09-16 14:24:45 +0300

    Developed sqlite implemntation

--- w-dms/app.go
+++ w-dms/app.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -45,7 +47,7 @@ type (
 	}
 )
 
-var db dbDriver // Yes, I temeber that global variable is bad. But I don't know yet what is good
+var db dbDriver // Yes, I remember that global variable is bad. But I don't know yet what is good
 
 func closeCompletelyRequestBody(body *io.ReadCloser) {
 	defer func() {
@@ -79,25 +81,101 @@ func writeTaskListToResponse(tasks TaskList, respWriter http.ResponseWriter) err
 	return err
 }
 
+func writeError(wr http.ResponseWriter, status int, err error) {
+	log.Println(err.Error())
+	wr.WriteHeader(status)
+	wr.Write([]byte(err.Error()))
+}
+
+func dispatchTaskRequest(respWriter http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodGet:
+		readTask(respWriter, request)
+	case http.MethodPost:
+		addTask(respWriter, request)
+	case http.MethodDelete:
+		deleteTask(respWriter, request)
+	case http.MethodPut:
+		updateTask(respWriter, request)
+	default:
+		writeError(respWriter, http.StatusNotFound, errors.New("Not found"))
+	}
+}
+
 func addTask(respWriter http.ResponseWriter, request *http.Request) {
 	task, err := readTaskFromRequest(request)
 	if err != nil {
-		respWriter.WriteHeader(http.StatusInternalServerError)
-		respWriter.Write([]byte(err.Error()))
+		writeError(respWriter, http.StatusInternalServerError, err)
+		return
 	}
 	err = db.Create(*task)
 	if err != nil {
-		log.Println(err.Error())
-		respWriter.WriteHeader(http.StatusInternalServerError)
-		respWriter.Write([]byte(err.Error()))
+		writeError(respWriter, http.StatusInternalServerError, err)
+		return
 	}
-	var res = TaskList{*task}
-	writeTaskListToResponse(res, respWriter)
+	respWriter.WriteHeader(http.StatusCreated)
+}
+
+func readTask(respWriter http.ResponseWriter, request *http.Request) {
+	var sID = request.FormValue("id")
+	var alias = request.FormValue("alias")
+	var result TaskList
+	var err error
+	if sID != "" {
+		id, parseErr := strconv.ParseInt(sID, 10, 0)
+		if parseErr != nil {
+			writeError(respWriter, http.StatusBadRequest, parseErr)
+			return
+		}
+		result, err = db.ReadByID(&id)
+	} else if alias != "" {
+		result, err = db.ReadByAlias(&alias)
+	} else {
+		result, err = db.ReadByID(nil)
+	}
+	if err != nil {
+		writeError(respWriter, http.StatusInternalServerError, err)
+		return
+	}
+	writeTaskListToResponse(result, respWriter)
+}
+
+func deleteTask(respWriter http.ResponseWriter, request *http.Request) {
+	var sID = request.FormValue("id")
+	if sID != "" {
+		id, parseErr := strconv.ParseInt(sID, 10, 0)
+		if parseErr != nil {
+			writeError(respWriter, http.StatusBadRequest, parseErr)
+			return
+		}
+		err := db.Delete(Task{ID: int(id)})
+		if err != nil {
+			writeError(respWriter, http.StatusInternalServerError, err)
+			return
+		}
+	} else {
+		writeError(respWriter, http.StatusBadRequest, errors.New("Task id is not specidied"))
+	}
+	respWriter.WriteHeader(http.StatusNoContent)
+}
+
+func updateTask(respWriter http.ResponseWriter, request *http.Request) {
+	task, err := readTaskFromRequest(request)
+	if err != nil {
+		writeError(respWriter, http.StatusInternalServerError, err)
+		return
+	}
+	err = db.Update(*task)
+	if err != nil {
+		writeError(respWriter, http.StatusInternalServerError, err)
+		return
+	}
+	respWriter.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
 	db = newSqliteDriver("/home/zhenya/Development/data/tasks.db")
 	var sm = http.NewServeMux()
-	sm.HandleFunc("/add", addTask)
+	sm.HandleFunc("/task", dispatchTaskRequest)
 	http.ListenAndServe(":8080", sm)
 }
--- w-dms/sqliteDriver.go
+++ w-dms/sqliteDriver.go
@@ -3,14 +3,28 @@ package main
 import (
 	"database/sql"
 	"log"
-	"time"
 )
 
-const selectCaluse = `select t.id, t.alias, t.description, t.task_timestamp, t.estimate_time, t.real_time, ty.type, r.reminder, ta.tag
+const (
+	insertClause = `insert into Tasks(alias, description, type_id, task_timestamp, estimate_time, real_time)
+ values(?, ?, ?, ?, ?, ?)`
+
+	selectCaluse = `select t.id, t.alias, t.description, t.task_timestamp, t.estimate_time, t.real_time, ty.type, r.reminder, ta.tag
 from Tasks t
 inner join Types ty on t.type_id = ty.id
 left join Tags ta on ta.task_id = t.id
 left join Reminders r on r.task_id = t.id`
+
+	updateClause = `update Tasks
+set alias = ?,
+    description = ?,
+    type_id = ?,
+    task_timestamp = ?,
+    estimate_time = ?,
+    real_time = ?
+where id = ?
+`
+)
 
 type sqliteDriver struct {
 	db *sql.DB
@@ -24,6 +38,107 @@ func newSqliteDriver(dbURL string) *sqliteDriver {
 	return &sqliteDriver{db}
 }
 
+func (d *sqliteDriver) Create(t Task) error {
+	var canCommit = false
+	transaction, err := d.db.Begin()
+	if err != nil {
+		return nil
+	}
+	defer func() {
+		if canCommit {
+			transaction.Commit()
+		} else {
+			transaction.Rollback()
+		}
+	}()
+	typeID, err := d.findTypeID(t.Type)
+	if err != nil {
+		return err
+	}
+	res, err := transaction.Exec(insertClause, t.Alias, t.Description, typeID, t.Timestamp, t.EstimateTime, t.RealTime)
+	if err != nil {
+		return err
+	}
+	taskID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	t.ID = int(taskID)
+	if err = addTaskTagsAndRemindersInTransaction(t, transaction); err != nil {
+		return err
+	}
+	canCommit = true
+	return nil
+}
+
+func (d *sqliteDriver) ReadByID(id *int64) (TaskList, error) {
+	if id != nil {
+		return d.readByCondition("where t.id = ?", id)
+	}
+	return d.readByCondition("")
+}
+
+func (d *sqliteDriver) ReadByAlias(alias *string) (TaskList, error) {
+	if alias != nil {
+		return d.readByCondition("where t.alias = ?", alias)
+	}
+	return d.readByCondition("")
+}
+
+func (d *sqliteDriver) Update(t Task) error {
+	var canCommit = false
+	transaction, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if canCommit {
+			transaction.Commit()
+		} else {
+			transaction.Rollback()
+		}
+	}()
+	if err = cleanTagsAndRemindersInTransaction(t, transaction); err != nil {
+		return err
+	}
+	typeID, err := d.findTypeID(t.Type)
+	if err != nil {
+		return err
+	}
+	_, err = transaction.Exec(updateClause, t.Alias, t.Description, typeID, t.Timestamp, t.EstimateTime, t.RealTime, t.ID)
+	if err != nil {
+		return err
+	}
+	if err = addTaskTagsAndRemindersInTransaction(t, transaction); err != nil {
+		return err
+	}
+	canCommit = true
+	return nil
+}
+
+func (d *sqliteDriver) Delete(t Task) error {
+	var canCommit = false
+	transaction, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if canCommit {
+			transaction.Commit()
+		} else {
+			transaction.Rollback()
+		}
+	}()
+	if err = cleanTagsAndRemindersInTransaction(t, transaction); err != nil {
+		return err
+	}
+	if _, err = transaction.Exec("delete from Tasks where id = ?", t.ID); err != nil {
+		return err
+	}
+	canCommit = true
+	return nil
+}
+
 func (d *sqliteDriver) findTypeID(typeName string) (int, error) {
 	row := d.db.QueryRow("select id from Types where type = ?", typeName)
 	var id int
@@ -34,72 +149,100 @@ func (d *sqliteDriver) findTypeID(typeName string) (int, error) {
 	return id, nil
 }
 
-func (d *sqliteDriver) Create(t Task) error {
-	transaction, err := d.db.Begin()
+func addTaskTagsAndRemindersInTransaction(t Task, tx *sql.Tx) error {
+	var tagStm, remStm *sql.Stmt
+	defer func() {
+		if tagStm != nil {
+			tagStm.Close()
+		}
+		if remStm != nil {
+			remStm.Close()
+		}
+	}()
+	tagStm, err := tx.Prepare("insert into Tags(task_id, tag) values(?, ?)")
 	if err != nil {
-		return nil
-	}
-	typeID, err := d.findTypeID(t.Type)
-	if err != nil {
-		transaction.Rollback()
-		return err
-	}
-	var timestamp = time.Now().Unix()
-	res, err := transaction.Exec(`insert into Tasks(alias, description, type_id, task_timestamp, estimate_time, real_time)
-   values(?, ?, ?, ?, ?, ?)`, t.Alias, t.Description, typeID, timestamp, t.EstimateTime, t.RealTime)
-	if err != nil {
-		transaction.Rollback()
-		return err
-	}
-	taskID, err := res.LastInsertId()
-	if err != nil {
-		transaction.Rollback()
 		return err
 	}
 	for _, tag := range t.Tags {
-		_, err = transaction.Exec("insert into Tags(task_id, tag) values(?, ?)", taskID, tag)
+		_, err = tagStm.Exec(t.ID, tag)
 		if err != nil {
-			transaction.Rollback()
 			return err
 		}
+	}
+	remStm, err = tx.Prepare("insert into Reminders(task_id, reminder) values(?, ?)")
+	if err != nil {
+		return err
 	}
 	for _, reminder := range t.Reminders {
-		_, err = transaction.Exec("insert into Reminders(task_id, reminder) values(?, ?)", taskID, reminder)
+		_, err = remStm.Exec(t.ID, reminder)
 		if err != nil {
-			transaction.Rollback()
 			return err
 		}
 	}
-	transaction.Commit()
 	return nil
 }
 
-func (d *sqliteDriver) ReadByID(id *int64) (TaskList, error) {
-	var taskMap = make(map[int]Task, 0)
+func cleanTagsAndRemindersInTransaction(task Task, tx *sql.Tx) error {
+	_, err := tx.Exec("delete from Tags where task_id = ?", task.ID)
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("delete from Reminders where task_id = ?", task.ID)
+	return err
+}
+
+func (d *sqliteDriver) readByCondition(condition string, args ...interface{}) (TaskList, error) {
+	var taskMap = make(map[int]*Task, 0)
 	var rows *sql.Rows
 	var err error
-	if id == nil {
-		rows, err = d.db.Query(selectCaluse)
-	} else {
-		rows, err = d.db.QueryRow(selectCaluse+" where t.id = ?", id)
-	}
+	rows, err = d.db.Query(selectCaluse+" "+condition, args...)
 	if err != nil {
 		return nil, err
 	}
+	var id int
+	var ts int64
+	var alias, typ string
+	var descr, rt, et, tag, rem sql.NullString
 	for rows.Next() {
-		// rows.Scan(dest)
+		err := rows.Scan(&id, &alias, &descr, &ts, &rt, &et, &typ, &rem, &tag)
+		if err != nil {
+			return nil, err
+		}
+		task, exists := taskMap[id]
+		if !exists {
+			task = &Task{ID: id, Alias: alias, Timestamp: ts, Type: typ, Reminders: []string{}, Tags: []string{}}
+			if descr.Valid {
+				task.Description = descr.String
+			}
+			if et.Valid {
+				task.EstimateTime = et.String
+			}
+			if rt.Valid {
+				task.RealTime = rt.String
+			}
+			taskMap[id] = task
+		}
+		if tag.Valid {
+			addToSliceIfAbsent(&task.Tags, tag.String)
+		}
+		if rem.Valid {
+			addToSliceIfAbsent(&task.Reminders, rem.String)
+		}
 	}
-	return nil, nil
+	var list = make(TaskList, len(taskMap))
+	var i int
+	for id = range taskMap {
+		list[i] = *taskMap[id]
+		i++
+	}
+	return list, nil
 }
 
-func (d *sqliteDriver) ReadByAlias(alias *string) (TaskList, error) {
-	return nil, nil
-}
-
-func (d *sqliteDriver) Update(t Task) error {
-	return nil
-}
-
-func (d *sqliteDriver) Delete(t Task) error {
-	return nil
+func addToSliceIfAbsent(slice *[]string, value string) {
+	for _, s := range *slice {
+		if s == value {
+			return
+		}
+	}
+	*slice = append(*slice, value)
 }
